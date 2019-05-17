7acbabc47e04cba709a6c9a940248ad236b2f21d
Travis <ga86yam@mytum.de>
2019-05-17 20:56:11 +0200

    add .eslintrc

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,25 +1,27 @@
-import * as AuthActions from '../actions/auth'
+import * as AuthActions from "../actions/auth";
 
 const initialState = {
-  token: null,
-  user: null,
-  error: null
-}
+	token: null,
+	user: null,
+	error: null,
+	// additional flag indicating that the user is undergoing email verifcation.
+	isVerifyingEmail: false
+};
 export default function authReducer(state = initialState, action) {
-  // list of possible attributes from action
-  const { token, user, error, info } = action
-  switch(action.type) {
-    case AuthActions.SET_TOKEN:
-      return {...state, error: null, token}
-    case AuthActions.SET_USER:
-      return {...state, error: null, user}
-    case AuthActions.SET_AUTH_ERROR:
-      return {...state, error}
-    case AuthActions.LOGOUT:
-      return {...initialState}
-    case AuthActions.UPDATE_USER_INFO:
-      return {...state, user: {...state.user, ...info}}
-    default:
-      return state
-  }
+	// list of possible attributes from action
+	const { token, user, error, info } = action;
+	switch (action.type) {
+		case AuthActions.SET_TOKEN:
+			return { ...state, error: null, token };
+		case AuthActions.SET_USER:
+			return { ...state, error: null, user };
+		case AuthActions.SET_AUTH_ERROR:
+			return { ...state, error };
+		case AuthActions.LOGOUT:
+			return { ...initialState };
+		case AuthActions.UPDATE_USER_INFO:
+			return { ...state, user: { ...state.user, ...info } };
+		default:
+			return state;
+	}
 }
