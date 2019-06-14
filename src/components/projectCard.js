d216d072c688e10c49b89fe5d5973052093d74d5
Travis <ga86yam@mytum.de>
2019-06-14 15:41:07 +0200

    Projects can be browsed by clicking the project list in home page

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -1,105 +1,116 @@
-import React from 'react'
-import {
-  Card,
-  CardTitle,
-  CardText,
-  Badge
-} from 'reactstrap'
-import { connect } from 'react-redux'
-import UserChip from './userChip'
-import * as ModalActions from '../actions/modal'
-import * as _ from 'lodash'
-import moment from 'moment'
-import { withRouter } from 'react-router-dom'
+import React from "react"
+import { Card, CardTitle, CardText, Badge } from "reactstrap"
+import { connect } from "react-redux"
+import UserChip from "./userChip"
+import * as ModalActions from "../actions/modal"
+import * as _ from "lodash"
+import moment from "moment"
+import { withRouter } from "react-router-dom"
 
 const ProjectCard = function(props) {
-  const {
-    style: customStyle = {},
-    project, isOwner,
-    color: customColor
-  } = props
-  if(!project) return null
-  const {
-    title,
-    creator,
-    status,
-    description,
-    tags: topic = [],
-    salary,
-    nature,
-    from, to,
-    _id: id,
-  } = project
-  if(!title || !creator) return null
-  const getStatusChip = (status) => {
-    let color = "info"
-    switch(status) {
-      case "open":
-        color = "success"
-        break
-      case "closed":
-        color = "danger"
-        break
-    }
-    return (<Badge color={color}> {status} </Badge>)
-  }
-  const formatDate = date => moment(date).format("MMM Do YY")
-  return (
-    <Card body inverse color={customColor || "secondary"}
-      onClick={() => props.history.push(`/project/${id}`)}
-      style={{...style.container, ...customStyle}}>
-      <CardTitle>
-        <div style={style.spread}>
-          <div>
-            <b>{title}</b> {getStatusChip(status)}
-          </div>
-          <div>
-            <h6>{formatDate(from)} { to && `- ${formatDate(to)}`} </h6>
-          </div>
-        </div>
-      </CardTitle>
-      {
-        !isOwner && (
-          <CardText>
-            <UserChip user={creator} />
-          </CardText>
-        )
-      }
-      <CardText>
-        {topic.map(t => (<Badge style={style.tagBadge} color="danger">{t}</Badge>))}
-      </CardText>
-      <CardText>
-        <div>
-          <div> {_.startCase(nature)} </div>
-          <div className="text-success"><b>{salary}€</b></div>
-      </div>
-      </CardText>
-    </Card>
-  )
+	const {
+		style: customStyle = {},
+		project,
+		isOwner,
+		color: customColor
+	} = props
+	if (!project) return null
+	const {
+		title,
+		creator,
+		status,
+		description,
+		tags: topic = [],
+		salary,
+		nature,
+		from,
+		to,
+		_id: id
+	} = project
+	if (!title || !creator) return null
+	const getStatusChip = status => {
+		let color = "info"
+		switch (status) {
+			case "open":
+				color = "success"
+				break
+			case "closed":
+				color = "danger"
+				break
+		}
+		return <Badge color={color}> {status} </Badge>
+	}
+	//() => props.history.push(`/project/${id}`)
+	const formatDate = date => moment(date).format("MMM Do YY")
+	return (
+		<Card
+			body
+			inverse
+			color={customColor || "secondary"}
+			onClick={() => alert("click projec!")}
+			style={{ ...style.container, ...customStyle }}>
+			<CardTitle>
+				<div style={style.spread}>
+					<div>
+						<b>{title}</b> {getStatusChip(status)}
+					</div>
+					<div>
+						<h6>
+							{formatDate(from)} {to && `- ${formatDate(to)}`}{" "}
+						</h6>
+					</div>
+				</div>
+			</CardTitle>
+			{!isOwner && (
+				<CardText>
+					<UserChip user={creator} />
+				</CardText>
+			)}
+			<CardText>
+				{topic.map(t => (
+					<Badge style={style.tagBadge} color="danger">
+						{t}
+					</Badge>
+				))}
+			</CardText>
+			<CardText>
+				<div>
+					<div> {_.startCase(nature)} </div>
+					<div className="text-success">
+						<b>{salary}€</b>
+					</div>
+				</div>
+			</CardText>
+		</Card>
+	)
 }
 
 const style = {
-  container: {
-    maxWidth: "35vw",
-    width: "35vw",
-    margin: 16
-  },
-  spread: {
-    display: 'flex',
-    justifyContent: 'space-between'
-  },
-  tagBadge: {
-    marginLeft: 4,
-    marginRight: 4
-  }
+	container: {
+		maxWidth: "35vw",
+		width: "35vw",
+		margin: 16
+	},
+	spread: {
+		display: "flex",
+		justifyContent: "space-between"
+	},
+	tagBadge: {
+		marginLeft: 4,
+		marginRight: 4
+	}
 }
 
 const mapDispatchToProps = dispatch => ({
-  showProject: p => dispatch({
-    type: ModalActions.MODIFY_PROJECT,
-    content: p
-  })
+	showProject: p =>
+		dispatch({
+			type: ModalActions.MODIFY_PROJECT,
+			content: p
+		})
 })
 export default withRouter(
-  connect(null,mapDispatchToProps)(ProjectCard)
+	connect(
+		null,
+		mapDispatchToProps
+	)(ProjectCard)
 )
--- src/pages/HomePage/frontPage.js
+++ src/pages/HomePage/frontPage.js
@@ -1,158 +1,122 @@
-import React from 'react'
-import CenterNotice from '../../components/centerNotice'
+import React from "react"
+import CenterNotice from "../../components/centerNotice"
+import { withRouter } from "react-router-dom"
 import {
-  TabContent, TabPane, Nav, NavItem, NavLink,
-  Card, Button, CardTitle, CardText,
-  Row, Col, Container
-} from 'reactstrap'
+	TabContent,
+	TabPane,
+	Nav,
+	NavItem,
+	NavLink,
+	Card,
+	Button,
+	CardTitle,
+	CardText,
+	Row,
+	Col,
+	Container
+} from "reactstrap"
 // import ProjectCard from '../../components/projectCard'
-import ProjectCard from '../../components/ProjectResultCard'
-import UserChip from '../../components/userChip'
-import Icon from '../../components/icon'
-import ApplicationCard from './applicationCard'
-import classnames from 'classnames'
-import {
-  getProject,
-  getApplications
-} from '../../backend/user'
-import Section from './Section'
+import ProjectCard from "../../components/ProjectResultCard"
+import UserChip from "../../components/userChip"
+import Icon from "../../components/icon"
+import ApplicationCard from "./applicationCard"
+import classnames from "classnames"
+import { getProject, getApplications } from "../../backend/user"
+import Section from "./Section"
 
-export default class FrontPage extends React.Component {
-  constructor(props) {
-    super(props)
-    // for studnets only
-    this.state = {
-      activeTab: "bookmark"
-    }
+class FrontPage extends React.Component {
+	constructor(props) {
+		super(props)
+		// for studnets only
+		this.state = {
+			activeTab: "bookmark"
+		}
+	}
+	// politicians' components
+	projectList(projects) {
+		return projects.map(p => (
+			<ProjectCard
+				style={style.projectCard}
+				project={p}
+				key={p._id}
+				onClick={() => this.props.history.push(`/project/${p._id}`)}
+				isOwner={true}
+			/>
+		))
+	}
 
-  }
-  // politicians' components
-  projectList(projects) {
-    return (
-      projects.map(p => (
-        <ProjectCard
-          style={style.projectCard}
-          project={p}
-          key={p._id}
-          isOwner={true} />
-      ))
-    )
-  }
+	// studnets' components
+	bookmarkTab(bookmarks) {
+		if (!bookmarks || !bookmarks.length) {
+			return <CenterNotice title="No bookmarks found" />
+		}
+		return bookmarks.map(p => (
+			<ProjectCard project={p} key={p._id} isOwner={false} />
+		))
+	}
+	applicationTab(applications) {
+		if (!applications || !applications.length) {
+			return <CenterNotice title="No applications found" />
+		}
+		return applications.map(app => <ApplicationCard application={app} />)
+	}
 
-  // studnets' components
-  bookmarkTab(bookmarks) {
-    if(!bookmarks || !bookmarks.length) {
-      return (
-        <CenterNotice title="No bookmarks found" />
-      )
-    }
-    return (
-        bookmarks.map(p => (
-          <ProjectCard
-            project={p}
-            key={p._id}
-            isOwner={false} />
-        ))
-    )
-  }
-  applicationTab(applications) {
-    if(!applications || !applications.length) {
-      return (
-        <CenterNotice title="No applications found" />
-      )
-    }
-    return applications.map(app => (
-          <ApplicationCard
-            application={app}
-          />)
-        )
-    // return (
-    //   <div style={style.content}>
-    //     {applications.map(app => (
-    //       <ApplicationCard
-    //         style={style.projectCard}
-    //         application={app}
-    //       />
-    //     ))}
-    //   </div>
-    // )
-  }
+	onGoingProjectsTab(projects) {
+		if (!projects) return null
+		const onGoingProjects = projects.filter(p => p.status == "active")
 
-  onGoingProjectsTab(projects) {
-    if(!projects) return null
-    const onGoingProjects =
-      projects.filter(p => p.status == "active")
+		return (
+			<Section icon="project" title="Ongoing Projects">
+				{this.projectList(onGoingProjects)}
+			</Section>
+		)
+	}
 
-    return (
-      <Section
-        icon="project"
-        title="Ongoing Projects">
-        {this.projectList(onGoingProjects)}
-      </Section>
-    )
-  }
+	render() {
+		const { isPolitician, bookmarks, applications, projects } = this.props.user
 
-  render() {
-    const {
-      isPolitician,
-      bookmarks,
-      applications,
-      projects
-    } = this.props.user
+		// students' front page
+		if (this.props.isUserHimself)
+			return (
+				<div style={style.container}>
+					<Container>
+						{isPolitician && projects && (
+							<Section title="Projects" icon="project">
+								{this.projectList(projects)}
+							</Section>
+						)}
+						{!isPolitician && bookmarks && (
+							<Section icon="bookmark" title="Bookmarks">
+								{this.bookmarkTab(bookmarks)}
+							</Section>
+						)}
 
-    // students' front page
-    if(this.props.isUserHimself)
-    return (
-      <div style={style.container}>
-        <Container>
-          {
-            isPolitician && projects && (
-              <Section
-                title="Projects"
-                icon="project">
-                {
-                  this.projectList(projects)
-                }
-              </Section>
-            )
-          }
-          {
-            !isPolitician && bookmarks && (
-              <Section
-                icon="bookmark"
-                title="Bookmarks">
-                {this.bookmarkTab(bookmarks)}
-              </Section>
-            )
-          }
-
-          <Section
-            icon="application"
-            title="Applications">
-            {this.applicationTab(applications)}
-          </Section>
-
-        </Container>
-      </div>
-    )
-    else {
-      return null
-    }
-  }
+						<Section icon="application" title="Applications">
+							{this.applicationTab(applications)}
+						</Section>
+					</Container>
+				</div>
+			)
+		else {
+			return null
+		}
+	}
 }
-const style = {
-  container: {
-    padding: 16
-  },
-  contentHeader: {
-    width: '100%'
-  },
-  content: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'flex-start',
-    flexWrap: 'wrap',
-    margin: 16
-  },
 
+export default withRouter(FrontPage)
+
+const style = {
+	container: {
+		padding: 16
+	},
+	contentHeader: {
+		width: "100%"
+	},
+	content: {
+		display: "flex",
+		justifyContent: "space-between",
+		alignItems: "flex-start",
+		flexWrap: "wrap",
+		margin: 16
+	}
 }
