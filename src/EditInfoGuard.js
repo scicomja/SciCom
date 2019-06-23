6b4b7122cad7929d5d894cb193493707f6910495
Travis <ga86yam@mytum.de>
2019-06-23 13:17:55 +0200

    Attempting to fix editInfo infinite loop

--- src/EditInfoGuard.js
+++ src/EditInfoGuard.js
@@ -1,23 +1,29 @@
 // component forcing the user to editinfo
-import React from 'react'
-import { withRouter } from 'react-router-dom'
-import { connect } from 'react-redux'
+import React from "react"
+import { withRouter } from "react-router-dom"
+import { connect } from "react-redux"
 
-const EditInfoGuard = (WrappedComponent) => {
-  class Guard extends React.Component {
-    render() {
-      const { children, user, history } = this.props
+const EditInfoGuard = WrappedComponent => {
+	class Guard extends React.Component {
+		render() {
+			const { children, user, history } = this.props
 
-      if(!user || !user.firstName || !user.lastName) {
-        history.push('/editInfo?block=true')
-      }
-      return <WrappedComponent {...this.props} />
-    }
-  }
-  return connect(mapStateToProps, null)(Guard)
+			if (!user) {
+				history.push("/")
+			}
+			if (!user.firstName || !user.lastName) {
+				history.push("/editInfo?block=true")
+			}
+			return <WrappedComponent {...this.props} />
+		}
+	}
+	return connect(
+		mapStateToProps,
+		null
+	)(Guard)
 }
 
 const mapStateToProps = state => ({
-  ...state.auth
+	...state.auth
 })
 export default EditInfoGuard
